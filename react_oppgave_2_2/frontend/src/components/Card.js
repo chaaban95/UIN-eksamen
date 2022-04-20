ca83307ae4d7ab718d879d92983d49bbee52a065
Omar <omarinio95@yahoo.com>
2022-04-20 16:50:24 +0200

    update

--- react_oppgave_2_2/frontend/src/components/Card.js
+++ react_oppgave_2_2/frontend/src/components/Card.js
@@ -1,30 +1,26 @@
 import { Link } from 'react-router-dom'
 
 export default function Card({ data, searchFilter }) {
+  const filtered = data.filter((service) =>
+    service.title.toLowerCase().includes(searchFilter.toLowerCase())
+  )
+  let filteredData = []
+
+  searchFilter ? (filteredData = filtered) : (filteredData = data)
   return (
     <section data-testid="services">
       {/* TODO: Bruk .map til å skrive ut tjenester */}
-      {data
-        .filter((service) => {
-          if (searchFilter == '') {
-            return service
-          } else if (
-            service.title.toLowerCase().includes(searchFilter.toLowerCase())
-          ) {
-            return service
-          }
-        })
-        .map((service) => (
-          <article key={service.slug} data-testid="service_wrapper">
-            <h3 data-testid="services_title">{service?.title}</h3>
-            <p data-testid="services_preamble">{service?.preAmble}</p>
-            <Link to={`/services/${service.slug}`} data-testid="services_url">
-              Til tjeneste
-            </Link>
-          </article>
-        ))}
+      {filteredData.map((service) => (
+        <article key={service.slug} data-testid="service_wrapper">
+          <h3 data-testid="services_title">{service?.title}</h3>
+          <p data-testid="services_preamble">{service?.preAmble}</p>
+          <Link to={`/services/${service.slug}`} data-testid="services_url">
+            Til tjeneste
+          </Link>
+        </article>
+      ))}
       {/* TODO: Hvis ingen tjenester ved filtrering. Vis denne */}
-      <p data-testid="empty">Ingen tjenester</p>
+      {filteredData.length === 0 && <p data-testid="empty">Ingen tjenester</p>}
     </section>
   )
 }
