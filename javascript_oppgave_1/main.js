792690606f19d7a500439a890ac002f42eee3715
PeteLeoi <peterbergersen66@gmail.com>
2022-04-26 12:21:40 +0200

    javascript 1

--- javascript_oppgave_1/main.js
+++ javascript_oppgave_1/main.js
@@ -1,3 +1,25 @@
-const startHere = "Her kommer din kode";
 
-"this is my first commit"
+
+let singleNumber = "";
+let digits = "";
+let realDigits = "";
+
+function buttonAdd() {
+    singleNumber = (Math.floor(1000 + Math.random() * 9000));
+    digits = singleNumber.toString().split('');
+    realDigits = digits.map(Number)
+    console.log(realDigits);
+    document.getElementById("numberOne").innerHTML = realDigits[0];
+    document.getElementById("numberTwo").innerHTML = realDigits[1];
+    document.getElementById("numberThree").innerHTML = realDigits[2];
+    document.getElementById("numberFour").innerHTML = realDigits[3];
+}
+
+function buttonSort() {
+    realDigits.sort(function(a, b){return a - b});
+    console.log("This sorts")
+    document.getElementById("numberOne").innerHTML = realDigits[0];
+    document.getElementById("numberTwo").innerHTML = realDigits[1];
+    document.getElementById("numberThree").innerHTML = realDigits[2];
+    document.getElementById("numberFour").innerHTML = realDigits[3];
+}
